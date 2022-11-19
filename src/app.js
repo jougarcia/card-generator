cedcf08e94efe64ed6ac9c847a305f646ad346b8
jougarcia <jougarcia95@gmail.com>
2022-11-19 13:47:51 +0000

    joseph

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+document.querySelector("#btn").addEventListener("click", generateCard);
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  generateCard();
+  document.getElementById("main").style.display = "block";
+  document.body.removeChild(document.getElementById("cargando"));
 };
+
+function generateCard() {
+  let figures = ["♦", "♥", "♠", "♣"];
+  let numFig = Math.floor(Math.random() * 3);
+  let figure = figures[numFig];
+  let color = numFig <= 1 ? "red" : "black";
+  let num = Math.floor(Math.random() * 12) + 1;
+  switch (num) {
+    case 1:
+      num = "A";
+      break;
+    case 11:
+      num = "J";
+      break;
+    case 12:
+      num = "Q";
+      break;
+    case 13:
+      num = "K";
+      break;
+  }
+  console.log(`Figura: ${figure} | Color: ${color} | Valor: ${num}`);
+  document.querySelectorAll(".figure").forEach(elm => (elm.innerHTML = figure));
+  document.getElementById("card").style.color = color;
+  document.getElementById("value").innerHTML = num;
+}
